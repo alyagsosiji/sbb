3ee3e007724cd9efdeddb97b43606a5442439b7b
pdd1230 <pddlim@gmail.com>
2026-08-12 16:27:41 +0900

    Entity class 설계+ h2 table 생성/JPA 환경설정

---
+++ src/main/java/com/example/sbb/Answer.java
@@ -0,0 +1,23 @@
+package com.example.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id   // answer entity --> pk id 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 신규 record가 insert 될때 자동 id값을 1씩 증가
+    private Integer id;
+
+    @Column(columnDefinition = "Text") // field 타입을 text로 지정
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
