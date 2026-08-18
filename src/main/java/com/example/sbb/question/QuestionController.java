c63998f8543a9984fb271a0f14cdeffd92b41de9
pdd1230 <pddlim@gmail.com>
2026-08-18 15:37:30 +0900

    답변 기능 만들기

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -5,28 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
-//    private final QuestionRepository questionRepository;
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    //URL 프리픽스
+    @GetMapping("/list")
     public String list(Model model) {
-//        List<Question> questionList = this.questionRepository.findAll();
         List<Question> questionList = this.questionService.getList();
 
-        // 중요 --> "questionList" 라는 이름이 html의 ${questionList}와 연결됨
         model.addAttribute("questionList", questionList);
         return "question_list";  // question_list.html  리턴 됨
     }
 
-    @GetMapping("question/detail/{id}")
+    @GetMapping("/detail/{id}")
     // @PathVariable
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
